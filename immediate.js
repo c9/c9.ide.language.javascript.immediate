9c70b6222ed1c71d77cceee5b5f0e511814d6e1b
Lennart Kats <lclkats@gmail.com>
2013-10-14 12:13:48 +0200

    Implement simple Immediate completer

--- immediate.js
+++ immediate.js
@@ -5,12 +5,28 @@
  * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
 define(function(require, exports, module) {
-    main.consumes = ["language"];
+    main.consumes = ["language", "tabManager"];
     main.provides = [];
     return main;
 
     function main(options, imports, register) {
         var language = imports.language;
+        var tabs = imports.tabManager;
+        var completeUtil = require("plugins/c9.ide.language/complete_util");
+    
+        var GET_ALL_PROPERTIES = function getAllProperties(obj) {
+            if (obj == null)
+                return [];
+            var results = [];
+            do {
+                var props = Object.getOwnPropertyNames(obj);
+                props.forEach(function(prop){
+                    if (results.indexOf(prop) === -1)
+                        results.push(prop);
+                });
+            } while (obj = Object.getPrototypeOf(obj));
+            return results;
+        }.toString();
 
         language.registerLanguageHandler('plugins/c9.ide.language.javascript.immediate/immediate_complete');
         language.registerLanguageHandler('plugins/c9.ide.language.javascript.immediate/immediate_complete_static');
@@ -18,16 +34,52 @@ define(function(require, exports, module) {
         language.on("initWorker", function(e) {
             var worker = e.worker;
             worker.on("js_immediate_complete", function(e) {
-                onImmediateComplete(e.immediateWindow, function(results) {
-                    worker.emit("js_immediate_complete_results", { data: results });
+                onImmediateComplete(e.data.immediateWindow, e.data.expr, function(results) {
+                    worker.emit("js_immediate_complete_results", { data: { results: results, id: e.data.id } });
                 });
             });
         });
+    
+        function onImmediateComplete(immediateWindow, expr, callback) {
+            var tab = tabs.findTab(immediateWindow);
+            if (!tab || !tab.editor || !tab.editor.ace || !tab.editor.ace.getSession())
+                return callback();
+            
+            var ace = tab.editor.ace;
+            var evaluator = ace.getSession().repl.evaluator;
+            var doc = ace.getSession().getDocument().getValue();
+            
+            var results;
+            var propMatch = doc.match(/(.*)\.([A-Za-z0-9*$_]*)$/);
+            var context;
+            var id;
+            if (propMatch) {
+                context = propMatch[1];
+                id = propMatch[2];
+            }
+            else {
+                context = "window";
+                id = doc.match(/[A-Za-z0-9*$_]*$/)[0] || "";
+            }
+            results = evaluator.evaluateHeadless(
+                "(" + GET_ALL_PROPERTIES + ")(" + context + ")"
+            );
+            if (!results.length) // error
+                callback();
+            
+            results = results.slice(); // make into real array
+            results = completeUtil.findCompletions(id, results);
+            callback(results.map(function(m) {
+                return {
+                  name        : m,
+                  replaceText : m,
+                  icon        : "property",
+                  meta        : "",
+                  priority    : m.match(/^_/) ? 1 : 2
+                };
+            }));
+        }
         
         register(null, {});
-    }
-    
-    function onImmediateComplete(immediateWindow, callback) {
-        callback([]);
     }
 });
--- immediate_complete.js
+++ immediate_complete.js
@@ -12,16 +12,52 @@ handler.handlesImmediate = function() {
     return this.HANDLES_IMMEDIATE;
 };
 
-handler.getCompletionRegex = function() {
-    return (/\$/);
+var requestId = 0;
+handler.complete = function(doc, fullAst, pos, currentNode, callback) {
+    if (!currentNode || !currentNode.getPos())
+        return callback();
+    
+    if (!isSafe(currentNode))
+        return callback();
+    
+    var expr = getExpression(doc, currentNode.getPos());
+    var myRequestId = ++requestId;
+    
+    this.sender.once("js_immediate_complete_results", function(e) {
+        if (myRequestId !== e.data.id)
+            return;
+        callback(e.data.results);
+    });
+    this.sender.emit(
+        "js_immediate_complete",
+        {
+            immediateWindow: this.immediateWindow,
+            expr: expr,
+            id: myRequestId
+        }
+    );
 };
 
-handler.complete = function(doc, fullAst, pos, currentNode, callback) {
-    this.sender.emit("js_immediate_complete", this.immediateWindow);
-    this.sender.once("js_immediate_complete_results", function(e) {
-        callback(e.data);
-    });
-};
+function isSafe(node) {
+    var badNodes = node.collectTopDown(
+        "Call(x, _)", function(b) {
+            return !b.x.rewrite('Var("require")');
+        }
+    );
+    return !badNodes.length && ["Call", "PropAccess", "Var"].indexOf(node.cons) > -1;
+}
+
+function getExpression(doc, pos) {
+    if (pos.sl === pos.el) {
+        return doc.getLine(pos.sl).substring(doc.sc, doc.ec);
+    }
+    var result = doc.getLine(pos.sl).substr(pos.sc);
+    for (var i = pos.sl + 1; i < pos.el; i++) {
+        result += doc.getLine(i);
+    }
+    result += doc.getLine(pos.el).substr(0, pos.ec);
+    return result;
+}
 
 
 });
